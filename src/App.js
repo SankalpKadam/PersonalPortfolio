de9caf2bc7b768c21f29d4fd5104dae8f4f2e17a
SankalpKadam <sankalptp07@gmail.com>
2024-01-11 14:28:35 -0600

    responsive about me completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './Components/Navbar/NavBar';
 import HomePage from './Components/LandingPage/HomePage'
+import AboutMe from './Components/LandingPage/AboutMe/AboutMe';
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <HomePage/>
+      <AboutMe />
     </div>
   );
 }
