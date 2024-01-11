bd97a0b9a85d146010ddd41c31a4261166af2867
SankalpKadam <sankalptp07@gmail.com>
2024-01-11 13:09:01 -0600

    Completed responsive info bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import NavBar from './Components/Navbar/NavBar';
-
+import HomePage from './Components/LandingPage/HomePage'
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <HomePage/>
     </div>
   );
 }
