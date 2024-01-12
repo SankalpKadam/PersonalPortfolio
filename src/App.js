9c0a2e62f04ea2d8b992da24f9b9a1ffae3a2954
SankalpKadam <sankalptp07@gmail.com>
2024-01-11 22:20:54 -0600

    Rectified the layout of the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import NavBar from './Components/Navbar/NavBar';
 import HomePage from './Components/LandingPage/HomePage'
-import AboutMe from './Components/LandingPage/AboutMe/AboutMe';
-import TopProjects from './Components/LandingPage/ProjectsPage/TopProjects';
 import Footer from './Components/LandingPage/Footer/Footer';
 
 function App() {
@@ -10,8 +8,6 @@ function App() {
     <div className="App">
       <NavBar/>
       <HomePage/>
-      <TopProjects/>
-      <AboutMe />
       <Footer/>
     </div>
   );
--- src/Components/LandingPage/HomePage.jsx
+++ src/Components/LandingPage/HomePage.jsx
@@ -1,37 +1,15 @@
 import React from 'react';
 import './Homepage.css';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TitlePage from './TitlePage/TitlePage';
+import TopProjects from './ProjectsPage/TopProjects';
+import AboutMe from './AboutMe/AboutMe';
+
 function Homepage() {
     return (
         <div className='homepage'>
-            <div className="homepage__Image">
-                <img src={require('./IMG_9151.png')} alt="My profile image" className='homepage__profileImage' />
-            </div>
-            <div className='homepage__smallInfo'>
-                <div className="homepage__smallName">
-                    <div>
-                        <span className="homepage__nameIntro">
-                            I am Sankalp Kadam
-                        </span>
-                        <span className="homepage__jobRoles">
-                            Software Developer
-                        </span>
-                    </div>
-                    
-                    <div className='homepage__socials'>
-                        <div className="homepage__contactBtn">
-                            Contact ME
-                        </div>
-                        <div className='homepage__socialIcon'>
-                            <LinkedInIcon fontSize='large' />
-                        </div>
-                        <div className='homepage__socialIcon'>
-                            <GitHubIcon fontSize='large'/>
-                        </div>
-                    </div>
-                </div>
-            </div>
+           <TitlePage />
+           <TopProjects/>
+           <AboutMe/>
         </div>
     )
 }
